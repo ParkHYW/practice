1544d99a55398748705de7f391f5b19bb31c4f63
ParkHYW <78480894+ParkHYW@users.noreply.github.com>
2021-05-22 21:59:48 +0900

    Delete mtvProject directory

--- mtvProject/mainApp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Blog(models.Model):
-    title = models.CharField(max_length=200)
-    writer = models.CharField(max_length=100)
-    pub_date = models.DateField()
-    body = models.TextField()
